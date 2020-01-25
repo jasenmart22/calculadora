cd9e28296d45b0c0b3f8a2636612aff631dd3af2
jasenmart <jasenmart@yahoo.es>
2020-01-25 10:44:29 +0100

    metodo sumar 2 elementos

--- src/main/java/com/calculadora/rest/CalculadoraRestController.java
+++ src/main/java/com/calculadora/rest/CalculadoraRestController.java
@@ -1,10 +1,17 @@
 package com.calculadora.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculadora")
 public class CalculadoraRestController {
-
+	
+	@GetMapping(value="/sumar/{op1}/{op2}")
+	public Double sumar(@PathVariable("op1") Double op1, @PathVariable("op2") Double op2) {
+		Double res=op1 + op2;
+		return res;		
+	}
 }

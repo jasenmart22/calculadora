abad8f883d5e9e9479c6c4bb7a2c760f294c4c8c
jasenmart <jasenmart@yahoo.es>
2020-01-25 11:15:47 +0100

    trazas de metodos con logger

--- src/main/java/com/calculadora/rest/CalculadoraRestController.java
+++ src/main/java/com/calculadora/rest/CalculadoraRestController.java
@@ -1,29 +1,29 @@
 package com.calculadora.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.corp.calculator.TracerImpl;
-
 @RestController
 @RequestMapping("/calculadora")
 public class CalculadoraRestController {
 	
-	TracerImpl tracer=new TracerImpl();
+	private static Logger LOG = LoggerFactory.getLogger(CalculadoraRestController.class);
 	
 	@GetMapping(value="/sumar/{op1}/{op2}")
 	public Double sumar(@PathVariable("op1") Double op1, @PathVariable("op2") Double op2) {
 		Double res=op1 + op2;
-		tracer.trace(op1 + " + " + op2 + " = " + res);
+		LOG.info(op1 + " + " + op2 + " = " + res);
 		return res;		
 	}
 	
 	@GetMapping(value="/restar/{op1}/{op2}")
 	public Double restar(@PathVariable("op1") Double op1, @PathVariable("op2") Double op2) {
 		Double res=op1 -op2;
-		tracer.trace(op1 + " - " + op2 + " = " + res);
+		LOG.info(op1 + " - " + op2 + " = " + res);
 		return res;
 	}
 }
